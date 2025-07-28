96070687dc6c66e82d83cf2cae81f40d6a964bb9
dan-willia <154013461+dan-willia@users.noreply.github.com>
2025-07-28 10:43:52 +0800

    Step 2: test database connection

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Daniel in 3308'
+
+@app.route('/db_test')
+def db_test():
+    conn = psychopg2.connect("postgresql://lab_10_db_f8wj_user:0vBxKQt9dbeeF7ej1aZHAqZejc3QhSpp@dpg-d23e6lfdiees739hrpcg-a/lab_10_db_f8wj")
+    conn.close()
+    return "Database Connection Successful"
