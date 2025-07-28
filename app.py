e352b1aaa2b4bb25d6f24a84ad9a1ac24f32d36e
dan-willia <154013461+dan-willia@users.noreply.github.com>
2025-07-28 11:38:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ def db_test():
     conn.close()
     return "Database Connection Successful"
 
-# @app.route('/db_create')
-# def db_create():
-#     conn = psycopg.connect("postgresql://lab_10_db_f8wj_user:0vBxKQt9dbeeF7ej1aZHAqZejc3QhSpp@dpg-d23e6lfdiees739hrpcg-a/lab_10_db_f8wj")
-#     cur = conn.cursor()
-#     cur.execute('''
-#         CREATE TABLE IF NOT EXISTS Basketball(
-#             First varchar(255),
-#             Last varchar(255),
-#             City varchar(255),
-#             Name varchar(255),
-#             Number int
-#             );
-#     ''')
-#     conn.commit()
-#     conn.close()
-#     return "Basketball table successfully created"
+@app.route('/db_create')
+def db_create():
+    conn = psycopg.connect("postgresql://lab_10_db_f8wj_user:0vBxKQt9dbeeF7ej1aZHAqZejc3QhSpp@dpg-d23e6lfdiees739hrpcg-a/lab_10_db_f8wj")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table successfully created"
